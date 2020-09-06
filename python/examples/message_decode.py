645b14cd1d1d41cb418d3b9ce34b58406db058c4
Adam Shapiro <adam@pointonenav.com>
2020-09-06 01:17:47 -0400

    Added Python message implementation.

---
+++ python/examples/message_decode.py
@@ -0,0 +1,56 @@
+from argparse import ArgumentParser
+import os
+import sys
+
+root_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_dir)
+
+from fusion_engine_client.messages.core import *
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('file', type=str, help="The path to a binary file to be read.")
+    options = parser.parse_args()
+
+    f = open(options.file, 'rb')
+
+    while True:
+        # Read the next message header.
+        data = f.read(MessageHeader.calcsize())
+        if len(data) == 0:
+            break
+
+        # Deserialize the header.
+        header = MessageHeader()
+        offset = header.unpack(buffer=data)
+
+        # Read the message payload and append it to the header.
+        data += f.read(header.payload_size_bytes)
+
+        # Validate the message length and CRC.
+        if len(data) != header.calcsize() + header.payload_size_bytes:
+            break
+        else:
+            header.validate_crc(data)
+
+        # Deserialize and print the message contents.
+        if header.message_type == PoseMessage.MESSAGE_TYPE:
+            contents = PoseMessage()
+            contents.unpack(buffer=data, offset=offset)
+
+            print('Pose message @ P1 time %s' % str(contents.p1_time))
+            print('  GPS time: %s' % str(contents.gps_time.as_gps()))
+            print('  LLA: %.6f, %.6f, %.3f (deg, deg, m)' % tuple(contents.lla_deg))
+            print('  YPR: %.2f, %.2f, %.2f (deg, deg, deg)' % tuple(contents.ypr_deg))
+        elif header.message_type == GNSSInfoMessage.MESSAGE_TYPE:
+            contents = GNSSInfoMessage()
+            contents.unpack(buffer=data, offset=offset)
+
+            print('GNSS info message @ P1 time %s' % str(contents.p1_time))
+            print('  GPS time: %s' % str(contents.gps_time.as_gps()))
+            print('  GDOP: %.1f' % contents.gdop)
+            print('  %d SVs:' % len(contents.svs))
+            for sv in contents.svs:
+                print('    %s PRN %d:' % (sv.system.name, sv.prn))
+                print('      Used in solution: %s' % ('yes' if sv.used_in_solution else 'no'))
+                print('      Az/el: %.1f, %.1f deg' % (sv.azimuth_deg, sv.elevation_deg))
---
+++ python/fusion_engine_client/messages/solution.py
@@ -0,0 +1,192 @@
+import struct
+from typing import List
+
+import numpy as np
+
+from .defs import *
+
+
+class PoseMessage:
+    """!
+    @brief Platform pose solution (position, velocity, attitude).
+    """
+    MESSAGE_TYPE = MessageType.POSE
+
+    _FORMAT = '<B ddd ddd ddd ddd'
+    _SIZE: int = struct.calcsize(_FORMAT)
+
+    def __init__(self):
+        self.p1_time = Timestamp()
+        self.gps_time = Timestamp()
+
+        self.solution_type = SolutionType.Invalid
+
+        self.lla_deg = np.full((3,), np.nan)
+        self.ypr_deg = np.full((3,), np.nan)
+        self.position_std_dev_ecef_m = np.full((3,), np.nan)
+
+        self.aggregate_protection_level_m = np.nan
+        self.horizontal_protection_level_m = np.nan
+        self.vertical_protection_level_m = np.nan
+
+    def pack(self, buffer: bytes = None, offset: int = 0, return_buffer: bool = True) -> (bytes, int):
+        if buffer is None:
+            buffer = bytes(self.calcsize())
+
+        initial_offset = offset
+
+        offset += self.p1_time.pack(buffer, offset, return_buffer=False)
+        offset += self.gps_time.pack(buffer, offset, return_buffer=False)
+
+        struct.pack_into(PoseMessage._FORMAT, buffer, offset,
+                         int(self.solution_type),
+                         self.lla_deg[0], self.lla_deg[1], self.lla_deg[2],
+                         self.ypr_deg[0], self.ypr_deg[1], self.ypr_deg[2],
+                         self.position_std_dev_ecef_m[0], self.position_std_dev_ecef_m[1],
+                         self.position_std_dev_ecef_m[2],
+                         self.aggregate_protection_level_m,
+                         self.horizontal_protection_level_m,
+                         self.vertical_protection_level_m)
+
+        if return_buffer:
+            return buffer
+        else:
+            return offset - initial_offset
+
+    def unpack(self, buffer: bytes, offset: int = 0) -> int:
+        initial_offset = offset
+
+        offset += self.p1_time.unpack(buffer, offset)
+        offset += self.gps_time.unpack(buffer, offset)
+
+        (solution_type_int,
+         self.lla_deg[0], self.lla_deg[1], self.lla_deg[2],
+         self.ypr_deg[0], self.ypr_deg[1], self.ypr_deg[2],
+         self.position_std_dev_ecef_m[0], self.position_std_dev_ecef_m[1],
+         self.position_std_dev_ecef_m[2],
+         self.aggregate_protection_level_m,
+         self.horizontal_protection_level_m,
+         self.vertical_protection_level_m) = \
+            struct.unpack_from(PoseMessage._FORMAT, buffer=buffer, offset=offset)
+
+        self.solution_type = SolutionType(solution_type_int)
+
+        return offset - initial_offset
+
+    @classmethod
+    def calcsize(cls) -> int:
+        return 2 * Timestamp.calcsize() + PoseMessage._SIZE
+
+
+class SatelliteInfo:
+    """!
+    @brief Information about an individual satellite.
+    """
+    _FORMAT = '<BB?dd'
+    _SIZE: int = struct.calcsize(_FORMAT)
+
+    def __init__(self):
+        self.system = SatelliteType.UNKNOWN
+        self.prn = 0
+        self.used_in_solution = False
+        self.azimuth_deg = np.nan
+        self.elevation_deg = np.nan
+
+    def pack(self, buffer: bytes = None, offset: int = 0, return_buffer: bool = True) -> (bytes, int):
+        args = (int(self.system), self.prn, self.used_in_solution, self.azimuth_deg, self.elevation_deg)
+        if buffer is None:
+            buffer = struct.pack(SatelliteInfo._FORMAT, *args)
+        else:
+            struct.pack_into(SatelliteInfo._FORMAT, buffer=buffer, offset=offset, *args)
+
+        if return_buffer:
+            return buffer
+        else:
+            return self.calcsize()
+
+    def unpack(self, buffer: bytes, offset: int = 0) -> int:
+        (system_int, self.prn, self.used_in_solution, self.azimuth_deg, self.elevation_deg) = \
+            struct.unpack_from(SatelliteInfo._FORMAT, buffer=buffer, offset=offset)
+        self.system = SatelliteType(system_int)
+        return self.calcsize()
+
+    @classmethod
+    def calcsize(cls) -> int:
+        return SatelliteInfo._SIZE
+
+
+class GNSSInfoMessage:
+    """!
+    @brief Information about the GNSS data used in the @ref PoseMessage with the corresponding timestamp.
+    """
+    MESSAGE_TYPE = MessageType.GNSS_INFO
+
+    INVALID_REFERENCE_STATION = 0xFFFFFFFF
+
+    _FORMAT = '<IddddH'
+    _SIZE: int = struct.calcsize(_FORMAT)
+
+    def __init__(self):
+        self.p1_time = Timestamp()
+        self.gps_time = Timestamp()
+
+        self.last_differential_time = Timestamp()
+
+        self.reference_station_id = GNSSInfoMessage.INVALID_REFERENCE_STATION
+
+        self.gdop = np.nan
+        self.pdop = np.nan
+        self.hdop = np.nan
+        self.vdop = np.nan
+
+        self.svs: List[SatelliteInfo] = []
+
+    def pack(self, buffer: bytes = None, offset: int = 0, return_buffer: bool = True) -> (bytes, int):
+        if buffer is None:
+            buffer = bytes(self.calcsize())
+
+        initial_offset = offset
+
+        offset += self.p1_time.pack(buffer, offset, return_buffer=False)
+        offset += self.gps_time.pack(buffer, offset, return_buffer=False)
+
+        offset += self.last_differential_time.pack(buffer, offset, return_buffer=False)
+
+        struct.pack_into(GNSSInfoMessage._FORMAT, buffer, offset,
+                         self.reference_station_id,
+                         self.gdop, self.pdop, self.hdop, self.vdop,
+                         len(self.svs))
+        offset += GNSSInfoMessage._SIZE
+
+        for sv in self.svs:
+            offset += sv.pack(buffer, offset, return_buffer=False)
+
+        if return_buffer:
+            return buffer
+        else:
+            return offset - initial_offset
+
+    def unpack(self, buffer: bytes, offset: int = 0) -> int:
+        initial_offset = offset
+
+        offset += self.p1_time.unpack(buffer, offset)
+        offset += self.gps_time.unpack(buffer, offset)
+
+        offset += self.last_differential_time.unpack(buffer, offset)
+
+        (self.reference_station_id,
+         self.gdop, self.pdop, self.hdop, self.vdop,
+         num_svs) = \
+            struct.unpack_from(GNSSInfoMessage._FORMAT, buffer=buffer, offset=offset)
+        offset += GNSSInfoMessage._SIZE
+
+        self.svs = []
+        for i in range(num_svs):
+            sv = SatelliteInfo()
+            offset += sv.unpack(buffer, offset)
+            self.svs.append(sv)
+
+        return offset - initial_offset
+
+    def calcsize(self) -> int:
+        return 3 * Timestamp.calcsize() + GNSSInfoMessage._SIZE + len(self.svs) * SatelliteInfo.calcsize()
