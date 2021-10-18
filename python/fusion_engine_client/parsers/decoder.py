f769da7200cd0e7fe5a628e402476ebb7f041f0a
jdiamond <feros32@gmail.com>
2021-10-18 14:37:11 -0700

    Improve testing.

--- python/fusion_engine_client/parsers/decoder.py
+++ python/fusion_engine_client/parsers/decoder.py
@@ -1,64 +1,68 @@
 from collections import defaultdict
 from typing import List, Dict, Callable, Optional, Tuple, Union
-from ..messages.internal import MessageHeader, MessageType, MessagePayload, message_type_to_class
+
+from ..messages import MessageHeader, MessageType, MessagePayload
+from ..messages.internal import message_type_to_class
 
 
 class FusionEngineDecoder:
-
     def __init__(self):
         self._buffer = bytearray()
         self._header: Optional[MessageHeader] = None
-        # TODO: Add wild card
-        self._callbacks: Dict[MessageType, List[Callable[[
-            MessageHeader, MessagePayload], None]]] = defaultdict(list)
+        # None in the callback key will match any MessageType.
+        self._callbacks: Dict[Optional[MessageType], List[Callable[[
+            MessageHeader, Union[MessagePayload, bytes]], None]]] = defaultdict(list)
 
-    def add_callback(self, MessageType, callback: Callable[[MessageHeader, MessagePayload], None]):
+    def add_callback(self, MessageType, callback: Callable[[Optional[MessageType], Union[MessagePayload, bytes]], None]):
         self._callbacks[MessageType].append(callback)
 
-    def on_data(self, data:Union[bytes, int]) -> List[Tuple[MessageHeader, MessagePayload]]:
+    def on_data(self, data: Union[bytes, int]) -> List[Tuple[MessageHeader, Union[MessagePayload, bytes]]]:
         if type(data) == int:
             data = data.to_bytes(1, 'big')
         self._buffer += data
-        return self._check_buffer()
 
-    def _check_buffer(self) -> List[Tuple[MessageHeader, MessagePayload]]:
-        if self._header is None:
-            while self._buffer:
-                if self._buffer[0] == MessageHeader._SYNC0:
+        decoded_messages = []
+        while self._buffer:
+            if self._header is None:
+                if self._buffer[0] != MessageHeader._SYNC0:
+                    self._buffer.pop(0)
+                    continue
+                if len(self._buffer) < 2:
                     break
-                self._buffer.pop(0)
-            if len(self._buffer) < 2:
-                return []
-            if self._buffer[1] != MessageHeader._SYNC1:
-                self._buffer.pop(0)
-                return self._check_buffer()
-            if len(self._buffer) < MessageHeader.calcsize():
-                return []
-            self._header = MessageHeader()
-            self._header.unpack(self._buffer)
-            if self._header.payload_size_bytes > MessageHeader._MAX_EXPECTED_SIZE_BYTES:
+                if self._buffer[1] != MessageHeader._SYNC1:
+                    self._buffer.pop(0)
+                    continue
+                if len(self._buffer) < MessageHeader.calcsize():
+                    break
+                self._header = MessageHeader()
+                self._header.unpack(self._buffer)
+                if self._header.payload_size_bytes > MessageHeader._MAX_EXPECTED_SIZE_BYTES:
+                    self._header = None
+                    self._buffer.pop(0)
+                    continue
+            msg_len = self._header.payload_size_bytes + MessageHeader.calcsize()
+            if len(self._buffer) < msg_len:
+                break
+            try:
+                self._header.validate_crc(self._buffer)
+                cls = message_type_to_class.get(
+                    self._header.message_type, None)
+                if cls is not None:
+                    contents = cls()
+                    contents.unpack(buffer=self._buffer,
+                                    offset=MessageHeader.calcsize())
+                    for callback in self._callbacks[self._header.message_type]:
+                        callback(self._header, contents)
+                    for callback in self._callbacks[None]:
+                        callback(self._header, contents)
+                    result = (self._header, contents)
+                else:
+                    result = (self._header, bytes(
+                        self._buffer[MessageHeader.calcsize():msg_len]))
+                self._header = None
+                self._buffer = self._buffer[msg_len:]
+                decoded_messages.append(result)
+            except Exception as e:
                 self._header = None
                 self._buffer.pop(0)
-                return self._check_buffer()
-        msg_len = self._header.payload_size_bytes + MessageHeader.calcsize()
-        if len(self._buffer) < msg_len:
-            return []
-        try:
-            self._header.validate_crc(self._buffer)
-            cls = message_type_to_class.get(self._header.message_type, None)
-            if cls is not None:
-                contents = cls()
-                contents.unpack(buffer=self._buffer,
-                                offset=MessageHeader.calcsize())
-                for callback in self._callbacks[self._header.message_type]:
-                    callback(self._header, contents)
-                result = (self._header, contents)
-            else:
-                result = (self._header, bytes(self._buffer[:msg_len]))
-            self._header = None
-            self._buffer = self._buffer[msg_len:]
-            return [result] + self._check_buffer()
-        except Exception as e:
-            self._header = None
-            self._buffer.pop(0)
-            return self._check_buffer()
+        return decoded_messages
--- python/tests/test_decoder.py
+++ python/tests/test_decoder.py
@@ -1,49 +1,90 @@
+from fusion_engine_client.messages import PoseMessage, PoseAuxMessage
+from fusion_engine_client.messages.defs import MessageHeader, MessageType
 from fusion_engine_client.parsers import FusionEngineDecoder
-
-from fusion_engine_client.messages.internal import ResetCommandMessage
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger('point_one').setLevel(logging.DEBUG)
 
-# reset message
-P1_RESET_MESSAGE = b'.1\x00\x00\xb4l\xd0T\x02\x00`N\x00\x00\x00\x00\x04\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\x00'
+
+P1_POSE_MESSAGE1 = b".1\x00\x00\xb3\x9a\xf0\x7f\x02\x00\x10'\x00\x00\x00\x00\x8c\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x08@\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f"
+P1_POSE_MESSAGE2 = b".1\x00\x00\x02O\xd6\xef\x02\x00\x10'\x01\x00\x00\x00\x8c\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x08@\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f"
+P1_POSE_AUX_MESSAGE3 = b".1\x00\x00z\x90\x98t\x02\x00\x13'\x02\x00\x00\x00\xa0\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\x00\x00\x00\x00\xf8\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f\x00\x00\xc0\x7f"
+
 
 def test_good_message_at_once():
   decoder = FusionEngineDecoder()
-  ret = decoder.on_data(P1_RESET_MESSAGE)
+  ret = decoder.on_data(P1_POSE_MESSAGE1)
   assert len(ret) == 1
-  assert ret[0][0].message_type == ResetCommandMessage.MESSAGE_TYPE
+  assert ret[0][0].message_type == PoseMessage.MESSAGE_TYPE
 
 def test_good_message_byte_by_byte():
   decoder = FusionEngineDecoder()
-  for byte in P1_RESET_MESSAGE[:-1]:
+  for byte in P1_POSE_MESSAGE1[:-1]:
     ret = decoder.on_data(byte)
     assert len(ret) == 0
-  ret = decoder.on_data(P1_RESET_MESSAGE[-1])
+  ret = decoder.on_data(P1_POSE_MESSAGE1[-1])
   assert len(ret) == 1
-  assert ret[0][0].message_type == ResetCommandMessage.MESSAGE_TYPE
+  assert ret[0][0].message_type == PoseMessage.MESSAGE_TYPE
 
 def test_multiple_good():
-  test_bytes = P1_RESET_MESSAGE + P1_RESET_MESSAGE
+  test_bytes = P1_POSE_MESSAGE1 + P1_POSE_MESSAGE2
   decoder = FusionEngineDecoder()
   ret = decoder.on_data(test_bytes)
   assert len(ret) == 2
-  assert ret[1][0].message_type == ResetCommandMessage.MESSAGE_TYPE
+  assert ret[0][0].message_type == PoseMessage.MESSAGE_TYPE
+  assert ret[0][0].sequence_number == 0
+  assert ret[1][0].message_type == PoseMessage.MESSAGE_TYPE
+  assert ret[1][0].sequence_number == 1
 
 def test_sync():
   # Bad preamble
-  test_bytes = bytearray() + P1_RESET_MESSAGE + P1_RESET_MESSAGE
+  test_bytes = bytearray() + P1_POSE_MESSAGE1 + P1_POSE_MESSAGE2
   test_bytes[0] = 1
   decoder = FusionEngineDecoder()
-  ret = decoder.on_data(P1_RESET_MESSAGE)
+  ret = decoder.on_data(test_bytes)
   assert len(ret) == 1
-  assert ret[0][0].message_type == ResetCommandMessage.MESSAGE_TYPE
+  assert ret[0][0].sequence_number == 1
 
   # CRC failure
-  test_bytes = bytearray() + P1_RESET_MESSAGE + P1_RESET_MESSAGE
+  test_bytes = bytearray() + P1_POSE_MESSAGE1 + P1_POSE_MESSAGE2
   test_bytes[25] = 1
   decoder = FusionEngineDecoder()
-  ret = decoder.on_data(P1_RESET_MESSAGE)
+  ret = decoder.on_data(test_bytes)
   assert len(ret) == 1
-  assert ret[0][0].message_type == ResetCommandMessage.MESSAGE_TYPE
+  assert ret[0][0].sequence_number == 1
+
+def test_unknown_message():
+  header = MessageHeader()
+  payload = b"1234"
+  header.message_type = MessageType.RESERVED
+  test_bytes = header.pack(payload=payload)
+  decoder = FusionEngineDecoder()
+  ret = decoder.on_data(test_bytes)
+  assert len(ret) == 1
+  assert ret[0][1] == payload
+
+def test_callbacks():
+  counters = [0, 0, 0, 0]
+
+  def func_helper(header, payload, idx):
+    counters[idx] += 1
+  func1 = lambda header, payload: func_helper(header, payload, 0)
+  func2 = lambda header, payload: func_helper(header, payload, 1)
+  func3 = lambda header, payload: func_helper(header, payload, 2)
+  func4 = lambda header, payload: func_helper(header, payload, 3)
+
+  test_bytes = P1_POSE_MESSAGE1 + P1_POSE_MESSAGE2 + P1_POSE_AUX_MESSAGE3
+  decoder = FusionEngineDecoder()
+  decoder.add_callback(PoseMessage.MESSAGE_TYPE, func1)
+  decoder.add_callback(PoseAuxMessage.MESSAGE_TYPE, func2)
+  decoder.add_callback(None, func3)
+  decoder.add_callback(PoseMessage.MESSAGE_TYPE, func4)
+  decoder.add_callback(None, func4)
+
+  decoder.on_data(test_bytes)
+
+  assert counters[0] == 2
+  assert counters[1] == 1
+  assert counters[2] == 3
+  assert counters[3] == 5
