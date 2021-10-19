eeb71982fa0623f41a3e4a4f5fe2a5da5dcea4f0
jdiamond <feros32@gmail.com>
2021-10-18 18:39:57 -0700

    Set source identifier for encoder.

--- python/fusion_engine_client/parsers/encoder.py
+++ python/fusion_engine_client/parsers/encoder.py
@@ -14,13 +14,14 @@ class FusionEngineEncoder:
     def __init__(self):
         self.sequence_number = 0
 
-    def encode_message(self, message: MessagePayload) -> (bytes):
+    def encode_message(self, message: MessagePayload, source_identifier=0) -> (bytes):
         """!
         @brief Serialize a message with valid header and payload.
 
         @post
-        The header will set the CRC, message type, sequence number, and message
-        version. The sequence_number is incremented each time this is called.
+        The header will set the CRC, message type, sequence number, source
+        identifier, and message version. The sequence_number is incremented
+        each time this is called.
 
         @param message The MessagePayload to serialize.
 
@@ -29,6 +30,7 @@ class FusionEngineEncoder:
         header = MessageHeader(message.get_type())
         header.message_version = message.get_version()
         header.sequence_number = self.sequence_number
+        header.source_identifier = source_identifier
         self.sequence_number += 1
 
         message_data = message.pack()
--- python/setup.py
+++ python/setup.py
@@ -9,10 +9,7 @@ setup(
     packages=find_packages(where='.'),
     install_requires=[
         'wheel>=0.36.2',
-        # TODO Temporarily install the patched aenum from my fork until https://github.com/ethanfurman/aenum/issues/4 is
-        # resolved.
-        #aenum>=3.0.0
-        'aenum @ git+https://github.com/adamshapiro0/aenum.git@extend-enum#egg=aenum',
+        'aenum @ git+https://github.com/PointOneNav/aenum.git@extend-enum#egg=aenum',
         'numpy>=1.16.0',
         'construct>=2.10.0',
     ],
