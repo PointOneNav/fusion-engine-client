46866240488d5d9133bb73cad6b688958f46eeeb
Adam Shapiro <adam@pointonenav.com>
2021-11-15 09:32:28 -0500

    Changed CRC check to early return to clarify code flow and added warning options.
    This works the same way as the previous version, but is a little more
    straight-forward to read for customers.
    
    It also adds constructor controls for gap and error warnings in favor of the
    previous print_seq_skip_warnings() function, which was inconsistent with the
    warn_on_unrecognized option.
    
    The warn_on_unrecognized behavior now warns _after_ CRC validation to avoid a
    flood of false positives.

--- python/fusion_engine_client/parsers/decoder.py
+++ python/fusion_engine_client/parsers/decoder.py
@@ -17,7 +17,7 @@ class FusionEngineDecoder:
     """
 
     def __init__(self, max_payload_len_bytes: int = MessageHeader._MAX_EXPECTED_SIZE_BYTES,
-                 warn_on_unrecognized: bool = True):
+                 warn_on_unrecognized: bool = False, warn_on_gap: bool = False, warn_on_error: bool = False):
         """!
         @brief Construct a new decoder instance.
 
@@ -26,14 +26,20 @@ class FusionEngineDecoder:
                resynchronization.
         @param warn_on_unrecognized If `True`, print a warning if a deserialized message header contains an unrecognized
                or unsupported message type.
+        @param warn_on_gap If `True`, print a warning if a gap is detected in the incoming message sequence numbers.
+        @param warn_on_error If `True`, print a warning if an error is detected (invalid CRC, invalid payload length,
+               etc.).
         """
         self._warn_on_unrecognized = warn_on_unrecognized
+        self._warn_on_seq_skip = warn_on_gap
+        self._warn_on_error = warn_on_error
+
         self._max_payload_len_bytes = max_payload_len_bytes
         self._buffer = bytearray()
         self._header: Optional[MessageHeader] = None
         self._msg_len = 0
-        self._bool_warn_seq_skip = False
         self._last_sequence_number = None
+
         self._callbacks: Dict[Optional[MessageType], List[Callable[[
             MessageHeader,
             Union[MessagePayload, bytes]], None]]] = defaultdict(list)
@@ -55,16 +61,6 @@ class FusionEngineDecoder:
         """
         # `self._callbacks` is a `defaultdict(list)` so no need to initialize list on first entry.
         self._callbacks[type].append(callback)
-
-    def print_seq_skip_warnings(self, enable):
-        """!
-        @brief Enable a warning to print if the received message headers skip
-               a sequence_number.
-
-        @param enable If this is `True` print the warning when a sequence
-               number is skipped.
-        """
-        self._bool_warn_seq_skip = enable
 
     def on_data(self, data: Union[bytes, int]) -> List[Tuple[MessageHeader, Union[MessagePayload, bytes]]]:
         """!
@@ -97,7 +93,7 @@ class FusionEngineDecoder:
             if len(self._buffer) < MessageHeader.calcsize():
                 break
             # Looking for a valid header.
-            if self._header is None:
+            elif self._header is None:
                 # Explicitly check for the first two sync bytes to be a bit more efficient then doing it inside the @ref
                 # MessageHeader.unpack() with an exception.
                 if self._buffer[0] != MessageHeader._SYNC0:
@@ -109,9 +105,12 @@ class FusionEngineDecoder:
 
                 # Possible header found. Decode it and wait for the payload.
                 self._header = MessageHeader()
-                self._header.unpack(self._buffer, warn_on_unrecognized=self._warn_on_unrecognized)
+                self._header.unpack(self._buffer, warn_on_unrecognized=False)
                 self._msg_len = self._header.payload_size_bytes + MessageHeader.calcsize()
                 if self._header.payload_size_bytes > self._max_payload_len_bytes:
+                    print_func = _logger.warning if self._warn_on_error else _logger.debug
+                    print_func('Message payload too big. [paylaod_size=%d B, max=%d B]',
+                               self._header.payload_size_bytes, self._max_payload_len_bytes)
                     self._header = None
                     self._buffer.pop(0)
                     continue
@@ -123,55 +122,69 @@ class FusionEngineDecoder:
             # Validate the CRC. This will raise an exception on CRC failure.
             try:
                 self._header.validate_crc(self._buffer)
-                # If cls is not None, it is a child of @ref MessagePayload that
-                # maps to the received @ref MessageType.
-                cls = message_type_to_class.get(
-                    self._header.message_type, None)
-                if cls is not None:
-                    contents = cls()
-                    try:
-                        contents.unpack(buffer=self._buffer,
-                                        offset=MessageHeader.calcsize())
-                    except Exception as e:
-                        _logger.error('Failed unpacking %s: %s',
-                                      self._header.message_type, e)
-                        self._header = None
-                        self._buffer.pop(0)
-                        continue
-                    result = (self._header, contents)
-                    _logger.debug(
-                        'Decoded FusionEngine message %s', repr(contents))
-                else:
-                    # Make a copy of payload to return instead of decoded
-                    # message.
-                    result = (self._header, bytes(
-                        self._buffer[MessageHeader.calcsize():self._msg_len]))
-                    _logger.debug('Decoded unknown FusionEngine message type=%d size=%d',
-                                  self._header.message_type, self._header.payload_size_bytes)
-                if self._last_sequence_number is not None and self._bool_warn_seq_skip and (self._last_sequence_number + 1) % 2**32 != self._header.sequence_number:
-                    _logger.warning("FusionEngine messages' sequence_number skipped from %d to %d.",
-                                    self._last_sequence_number, self._header.sequence_number)
-                self._last_sequence_number = self._header.sequence_number
-                # self._callbacks is a `defaultdict(list)` and returns []
-                # if the key is not found.
-                for callback in self._callbacks[self._header.message_type]:
-                    # Map results to callback parameters.
-                    callback(*result)
-                # Match None callbacks to every message.
-                for callback in self._callbacks[None]:
-                    # Map results to callback parameters.
-                    callback(*result)
-                # Move decoder past the current message
-                self._buffer = self._buffer[self._msg_len:]
-                self._header = None
-                self._msg_len = 0
-                decoded_messages.append(result)
-            # Thrown on bad CRC or if the `unpack` function for a known
-            # @ ref MessagePayload fails (ie. the header payload length differs
-            # from the length assumed by the `unpack` function).
+            # Invalid CRC detected.
             except Exception as e:
-                _logger.debug(e)
+                print_func = _logger.warning if self._warn_on_error else _logger.debug
+                print_func(e)
                 self._header = None
                 self._msg_len = 0
                 self._buffer.pop(0)
+                continue
+
+            # Check for sequence number gaps.
+            if self._last_sequence_number is not None and self._warn_on_seq_skip:
+                expected_sequence_number = (self._last_sequence_number + 1) % 2**32
+                if self._header.sequence_number != expected_sequence_number:
+                    _logger.warning("Gap detected in FusionEngine message sequence numbers. [expected=%d, "
+                                    "received=%d].",
+                                    expected_sequence_number, self._header.sequence_number)
+            self._last_sequence_number = self._header.sequence_number
+
+            # Get the class for the received message type and deserialize the message payload. If cls is not None, it is
+            # a child of @ref MessagePayload that maps to the received @ref MessageType.
+            cls = message_type_to_class.get(self._header.message_type, None)
+            if cls is not None:
+                contents = cls()
+                try:
+                    contents.unpack(buffer=self._buffer, offset=MessageHeader.calcsize())
+                except Exception as e:
+                    # unpack() may fail if the payload length in the header differs from the length expected by the
+                    # class, the payload contains an illegal value, etc.
+                    _logger.error('Error deserializing message %s payload: %s', self._header.get_type_string(), e)
+                    self._header = None
+                    self._msg_len = 0
+                    self._buffer.pop(0)
+                    continue
+                result = (self._header, contents)
+                _logger.debug('Decoded FusionEngine message %s.', repr(contents))
+            # If cls is None, we don't have a class for the message type. Return a copy of the payload bytes.
+            else:
+                result = (self._header, bytes(self._buffer[MessageHeader.calcsize():self._msg_len]))
+                print_func = _logger.warning if self._warn_on_unrecognized else _logger.debug
+                print_func('Decoded unknown FusionEngine message. [type=%d, payload_size=%d B]',
+                           self._header.message_type, self._header.payload_size_bytes)
+
+            # Store the result.
+            decoded_messages.append(result)
+
+            # Call any callbacks registered for this message type.
+            #
+            # self._callbacks is a `defaultdict(list)` and returns an empty list ([]) if the key is not found.
+            for callback in self._callbacks[self._header.message_type]:
+                # Unpack results so that the callback is called as:
+                #   callback(header, contents)
+                callback(*result)
+
+            # Callbacks registered with type None should receive all message types.
+            for callback in self._callbacks[None]:
+                # Unpack results so that the callback is called as:
+                #   callback(header, contents)
+                callback(*result)
+
+            # Move decoder past the current message.
+            self._buffer = self._buffer[self._msg_len:]
+            self._header = None
+            self._msg_len = 0
+
+        # Return the list of decoded messages.
         return decoded_messages
