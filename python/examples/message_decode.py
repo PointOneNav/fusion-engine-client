6fa285aa244fa1d730f0718e59031702c8579e6d
Adam Shapiro <adam@pointonenav.com>
2022-01-17 09:15:17 -0500

    Modified message decode example to use the decoder and support mixed content.

--- python/examples/message_decode.py
+++ python/examples/message_decode.py
@@ -7,55 +7,8 @@ import sys
 root_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
 
-from fusion_engine_client.messages.core import *
-from fusion_engine_client.messages import ros
-
-
-def decode_message(header, data, offset):
-    # Validate the message length and CRC.
-    if len(data) != header.calcsize() + header.payload_size_bytes:
-        return False
-    else:
-        header.validate_crc(data)
-
-    # Check that the sequence number increments as expected.
-    if header.sequence_number != decode_message.expected_sequence_number:
-        print('Warning: unexpected sequence number. [expected=%d, received=%d]' %
-              (decode_message.expected_sequence_number, header.sequence_number))
-
-    decode_message.expected_sequence_number = header.sequence_number + 1
-
-    # Deserialize and print the message contents.
-    #
-    # Note: This could also be done more generally using the fusion_engine_client.message_type_to_class dictionary.
-    # We do it explicitly here for sake of example.
-    if header.message_type == PoseMessage.MESSAGE_TYPE:
-        contents = PoseMessage()
-        contents.unpack(buffer=data, offset=offset)
-    elif header.message_type == GNSSInfoMessage.MESSAGE_TYPE:
-        contents = GNSSInfoMessage()
-        contents.unpack(buffer=data, offset=offset)
-    elif header.message_type == GNSSSatelliteMessage.MESSAGE_TYPE:
-        contents = GNSSSatelliteMessage()
-        contents.unpack(buffer=data, offset=offset)
-    elif header.message_type == ros.PoseMessage.MESSAGE_TYPE:
-        contents = ros.PoseMessage()
-        contents.unpack(buffer=data, offset=offset)
-    elif header.message_type == ros.GPSFixMessage.MESSAGE_TYPE:
-        contents = ros.GPSFixMessage()
-        contents.unpack(buffer=data, offset=offset)
-    elif header.message_type == ros.IMUMessage.MESSAGE_TYPE:
-        contents = ros.IMUMessage()
-        contents.unpack(buffer=data, offset=offset)
-    else:
-        contents = None
-
-    print_message(header, contents)
-
-    return True
-
-
-decode_message.expected_sequence_number = 0
+from fusion_engine_client.messages.core import MessagePayload
+from fusion_engine_client.parsers import FusionEngineDecoder
 
 
 def print_message(header, contents):
@@ -70,29 +23,23 @@ def print_message(header, contents):
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="""\
-Manually decode and print the contents of messages contained in a *.p1log file.
+Decode and print the contents of messages contained in a *.p1log file or other
+binary file containing FusionEngine messages. The binary file may also contain
+other types of data.
 """)
     parser.add_argument('file', type=str, help="The path to a binary file to be read.")
     options = parser.parse_args()
 
     f = open(options.file, 'rb')
 
+    decoder = FusionEngineDecoder()
     while True:
         # Read the next message header.
-        data = f.read(MessageHeader.calcsize())
+        data = f.read(1024)
         if len(data) == 0:
             break
 
-        # Deserialize the header.
-        try:
-            header = MessageHeader()
-            offset = header.unpack(buffer=data)
-        except Exception as e:
-            print('Decode error: %s' % str(e))
-            break
-
-        # Read the message payload and append it to the header.
-        data += f.read(header.payload_size_bytes)
-
-        if not decode_message(header, data, offset):
-            break
+        # Decode the incoming data and print the contents of any complete messages.
+        messages = decoder.on_data(data)
+        for (header, message) in messages:
+            print_message(header, message)
