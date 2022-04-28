09647933e5af8398c6686d839048984bd24957e7
Adam Shapiro <adam@pointonenav.com>
2022-04-28 16:25:22 -0400

    Use locate_log() in all example apps to support mixed content and log searching.

--- python/examples/analyze_data.py
+++ python/examples/analyze_data.py
@@ -10,24 +10,42 @@ sys.path.append(root_dir)
 
 from fusion_engine_client.analysis.file_reader import FileReader
 from fusion_engine_client.messages.core import *
+from fusion_engine_client.utils.log import locate_log, DEFAULT_LOG_BASE_DIR
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="""\
 Compute the average LLA position for the data contained in a *.p1log file.
-
-Note that the specified file must contain only FusionEngine messages. For logs
-containing mixed content, see separate_mixed_log.py.
 """)
-    parser.add_argument('file', type=str, help="The path to a binary file to be read.")
+    parser.add_argument('--log-base-dir', metavar='DIR', default=DEFAULT_LOG_BASE_DIR,
+                        help="The base directory containing FusionEngine logs to be searched if a log pattern is "
+                             "specified.")
+    parser.add_argument('log',
+                        help="The log to be read. May be one of:\n"
+                             "- The path to a .p1log file or a file containing FusionEngine messages and other "
+                             "content\n"
+                             "- The path to a FusionEngine log directory\n"
+                             "- A pattern matching a FusionEngine log directory under the specified base directory "
+                             "(see find_fusion_engine_log() and --log-base-dir)")
     options = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s')
     logger = logging.getLogger('point_one.fusion_engine')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
+
+    # Locate the input file and set the output directory.
+    input_path, output_dir, log_id = locate_log(options.log, return_output_dir=True, return_log_id=True,
+                                                log_base_dir=options.log_base_dir)
+    if input_path is None:
+        sys.exit(1)
+
+    if log_id is None:
+        logger.info('Loading %s.' % os.path.basename(input_path))
+    else:
+        logger.info('Loading %s from log %s.' % (os.path.basename(input_path), log_id))
 
     # Read pose data from the file.
-    reader = FileReader(options.file)
+    reader = FileReader(input_path)
     result = reader.read(message_types=[PoseMessage], show_progress=True)
 
     # Print out the messages that were read.
