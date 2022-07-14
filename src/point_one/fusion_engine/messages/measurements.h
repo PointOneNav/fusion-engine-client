3aaee18005b45213c632afc05d74c8d19284b58a
Adam Shapiro <adam@pointonenav.com>
2022-07-14 11:17:31 -0400

    Added incoming sensor measurement timestamp definition.

--- src/point_one/fusion_engine/messages/measurements.h
+++ src/point_one/fusion_engine/messages/measurements.h
@@ -26,6 +26,85 @@ namespace messages {
  */
 
 /**
+ * @brief The source of a @ref TimeStamp used to represent the time of
+ *        applicability of an incoming sensor measurement.
+ * @ingroup measurement_messages
+ */
+enum class SystemTimeSource : uint8_t {
+  /** Timestamp not valid. */
+  INVALID = 0,
+  /** Message timestamped in P1 time. */
+  P1_TIME = 1,
+  /**
+   * Message timestamped in system time, generated when received by the device.
+   */
+  TIMESTAMPED_ON_RECEPTION = 2,
+  /**
+   * Message timestamp was generated from a monotonic clock of an external
+   * system.
+   */
+  SENDER_SYSTEM_TIME = 3,
+};
+
+/**
+ * @brief Get a human-friendly string name for the specified @ref
+ *        SystemTimeSource.
+ *
+ * @param val The enum to get the string name for.
+ *
+ * @return The corresponding string name.
+ */
+inline const char* to_string(SystemTimeSource val) {
+  switch (val) {
+    case SystemTimeSource::INVALID:
+      return "Invalid";
+    case SystemTimeSource::P1_TIME:
+      return "P1Time";
+    case SystemTimeSource::TIMESTAMPED_ON_RECEPTION:
+      return "Timestamped on Reception";
+    case SystemTimeSource::SENDER_SYSTEM_TIME:
+      return "Sender System Time";
+    default:
+      return "Unrecognized";
+  }
+}
+
+/**
+ * @brief @ref SystemTimeSource stream operator.
+ */
+inline std::ostream& operator<<(std::ostream& stream, SystemTimeSource val) {
+  stream << to_string(val) << " (" << (int)val << ")";
+  return stream;
+}
+
+/**
+ * @brief The time of applicability for an incoming sensor measurement.
+ *
+ * By convention this will be the first member of any measurement definition
+ * intended to be externally sent by the user to the device. On most platforms,
+ * incoming sensor measurements are timestamped by the device when they arrive.
+ * On some platforms, incoming sensor measurements may be timestamped externally
+ * by the user prior to arrival.
+ */
+struct alignas(4) MeasurementTimestamps {
+  /**
+   * The time of the message, if available. The source of this value is
+   * specified in @ref measurement_time_source.
+   */
+  Timestamp measurement_time;
+
+  /**
+   * The source for @ref measurement_time.
+   */
+  SystemTimeSource measurement_time_source = SystemTimeSource::INVALID;
+
+  uint8_t reserved[3] = {0};
+
+  /** The GPS time of the message, if available, referenced to 1980/1/6. */
+  Timestamp gps_time;
+};
+
+/**
  * @brief IMU sensor measurement data (@ref MessageType::IMU_MEASUREMENT,
  *        version 1.0).
  * @ingroup measurement_messages
