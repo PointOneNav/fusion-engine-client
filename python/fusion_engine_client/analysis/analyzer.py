666fcf42435f4d580c71c4e9fd5271aeea52b43c
Adam Shapiro <adam@pointonenav.com>
2022-07-21 13:30:18 -0400

    Added wheel tick plotting support.

--- python/fusion_engine_client/analysis/analyzer.py
+++ python/fusion_engine_client/analysis/analyzer.py
@@ -697,6 +697,136 @@ class Analyzer(object):
 
         self._add_figure(name="map", figure=figure, title="Vehicle Trajectory (Map)")
 
+    def plot_wheel_data(self):
+        """!
+        @brief Plot wheel tick/speed data.
+        """
+        if self.output_dir is None:
+            return
+
+        self._plot_wheel_ticks()
+
+    def _plot_wheel_ticks(self):
+        """!
+        @brief Plot wheel tick data.
+        """
+        # Read the data.
+        result = self.reader.read(message_types=[WheelTickMeasurement, VehicleTickMeasurement], **self.params)
+
+        wheel_data = result[WheelTickMeasurement.MESSAGE_TYPE]
+        if len(wheel_data.p1_time) == 0:
+            wheel_data = None
+
+        vehicle_data = result[VehicleTickMeasurement.MESSAGE_TYPE]
+        if len(vehicle_data.p1_time) == 0:
+            vehicle_data = None
+
+        if wheel_data is None and vehicle_data is None:
+            self.logger.info('No wheel tick data available. Skipping plot.')
+            return
+
+        # Setup the figure.
+        figure = make_subplots(rows=3, cols=1, print_grid=False, shared_xaxes=True,
+                               subplot_titles=['Tick Count', 'Tick Rate', 'Gear/Direction'])
+
+        figure['layout'].update(showlegend=True, modebar_add=['v1hovermode', 'toggleSpikelines'])
+        figure['layout']['xaxis1'].update(title="Time (sec)", showticklabels=True)
+        figure['layout']['xaxis2'].update(title="Time (sec)", showticklabels=True)
+        figure['layout']['yaxis1'].update(title="Tick Count")
+        figure['layout']['yaxis2'].update(title="Tick Rate (ticks/s)")
+        figure['layout']['yaxis3'].update(title="Gear/Direction",
+                                          ticktext=['%s (%d)' % (e.name, e.value) for e in GearType],
+                                          tickvals=[e.value for e in GearType])
+
+        # Check if the data has P1 time available. If not, we'll plot in the original source time.
+        wheel_time_source = None
+        vehicle_time_source = None
+
+        if wheel_data is not None:
+            if np.all(np.isnan(wheel_data.p1_time)):
+                if np.any(np.diff(wheel_data.measurement_time_source) != 0):
+                    self.logger.warning('Detected multiple time source types in wheel tick data.')
+
+                wheel_time_source = SystemTimeSource(wheel_data.measurement_time_source[0])
+                self.logger.warning('Wheel tick data does not have P1 time available. Plotting in %s time.' %
+                                    self._time_source_to_display_name(wheel_time_source))
+            else:
+                wheel_time_source = SystemTimeSource.P1_TIME
+
+            figure['layout']['annotations'][0]['text'] += 'Wheel tick time source: %s' % \
+                                                          self._time_source_to_display_name(wheel_time_source)
+
+        if vehicle_data is not None:
+            if np.all(np.isnan(vehicle_data.p1_time)):
+                if np.any(np.diff(vehicle_data.measurement_time_source) != 0):
+                    self.logger.warning('Detected multiple time source types in vehicle tick data.')
+
+                vehicle_time_source = SystemTimeSource(vehicle_data.measurement_time_source[0])
+                self.logger.warning('Vehicle tick data does not have P1 time available. Plotting in %s time.' %
+                                    self._time_source_to_display_name(vehicle_time_source))
+            else:
+                vehicle_time_source = SystemTimeSource.P1_TIME
+
+            figure['layout']['annotations'][0]['text'] += 'Vehicle tick time source: %s' % \
+                                                          self._time_source_to_display_name(vehicle_time_source)
+
+        if wheel_time_source is not None and vehicle_time_source is not None:
+            if wheel_time_source != vehicle_time_source:
+                self.logger.warning('Both wheel and vehicle tick data present, but timestamped with different sources. '
+                                    'Plotted data may not align in time.')
+
+        # Plot the data.
+        def _plot_trace(time, data, name, color, text):
+            figure.add_trace(go.Scattergl(x=time, y=data, text=text,
+                                          name=name, legendgroup=name,
+                                          mode='markers', marker={'color': color}),
+                             1, 1)
+
+            dt_sec = np.diff(time)
+            ticks_per_sec = np.diff(data) / dt_sec
+            figure.add_trace(go.Scattergl(x=time[:1], y=ticks_per_sec, text=text,
+                                          name=name, legendgroup=name, showlegend=False,
+                                          mode='markers', marker={'color': color}),
+                             2, 1)
+
+        if wheel_data is not None:
+            abs_time_sec = self._get_measurement_time(wheel_data)
+            t0 = self._get_t0_for_time_source(wheel_time_source)
+            time = abs_time_sec - t0
+            time_name = self._time_source_to_display_name(wheel_time_source)
+            text = ["%s Time: %.3f sec" % (time_name, t) for t in abs_time_sec]
+
+            _plot_trace(time=time, data=wheel_data.front_left_wheel_ticks, text=text,
+                        name='Front Left Wheel', color='red')
+            _plot_trace(time=time, data=wheel_data.front_right_wheel_ticks, text=text,
+                        name='Front Right Wheel', color='green')
+            _plot_trace(time=time, data=wheel_data.rear_left_wheel_ticks, text=text,
+                        name='Rear Left Wheel', color='blue')
+            _plot_trace(time=time, data=wheel_data.rear_right_wheel_ticks, text=text,
+                        name='Rear Right Wheel', color='purple')
+
+            figure.add_trace(go.Scattergl(x=time, y=wheel_data.gear, text=text,
+                                          name='Gear (Wheel Data)',
+                                          mode='markers', marker={'color': 'red'}),
+                             3, 1)
+
+        if vehicle_data is not None:
+            abs_time_sec = self._get_measurement_time(vehicle_data)
+            t0 = self._get_t0_for_time_source(vehicle_time_source)
+            time = abs_time_sec - t0
+            time_name = self._time_source_to_display_name(vehicle_time_source)
+            text = ["%s Time: %.3f sec" % (time_name, t) for t in abs_time_sec]
+
+            _plot_trace(time=time, data=vehicle_data.tick_count, text=text,
+                        name='Front Left Wheel', color='orange')
+
+            figure.add_trace(go.Scattergl(x=time, y=vehicle_data.gear, text=text,
+                                          name='Gear (Vehicle Data)',
+                                          mode='markers', marker={'color': 'orange'}),
+                             3, 1)
+
+        self._add_figure(name="wheel_ticks", figure=figure, title="Wheel Encoder Ticks")
+
     def plot_imu(self):
         """!
         @brief Plot the IMU data.
@@ -970,6 +1100,34 @@ class Analyzer(object):
 
         return None
 
+    def _get_t0_for_time_source(self, time_source: SystemTimeSource) -> float:
+        if time_source == SystemTimeSource.P1_TIME:
+            return float(self.t0)
+        elif time_source == SystemTimeSource.GPS_TIME:
+            return 0.0
+        elif time_source == SystemTimeSource.SENDER_SYSTEM_TIME:
+            return 0.0
+        elif time_source == SystemTimeSource.TIMESTAMPED_ON_RECEPTION:
+            return float(self.system_t0)
+
+    @classmethod
+    def _get_measurement_time(cls, data, time_source: SystemTimeSource) -> np.ndarray:
+        if time_source == SystemTimeSource.P1_TIME:
+            return data.p1_time
+        else:
+            return data.measurement_time
+
+    @classmethod
+    def _time_source_to_display_name(cls, time_source: SystemTimeSource) -> str:
+        if time_source == SystemTimeSource.P1_TIME:
+            return 'P1'
+        elif time_source == SystemTimeSource.GPS_TIME:
+            return 'GPS'
+        elif time_source == SystemTimeSource.SENDER_SYSTEM_TIME:
+            return 'External'
+        elif time_source == SystemTimeSource.TIMESTAMPED_ON_RECEPTION:
+            return 'System'
+
 
 def main():
     parser = ArgumentParser(description="""\
@@ -982,12 +1140,12 @@ Load and display information stored in a FusionEngine binary file.
                         help="If set, do not load the .p1i index file corresponding with the .p1log data file. If "
                              "specified and a .p1i file does not exist, do not generate one. Otherwise, a .p1i file "
                              "will be created automatically to improve data read speed in the future.")
-    parser.add_argument('--imu', action='store_true',
-                        help="Plot IMU data (slow).")
     parser.add_argument('--mapbox-token', metavar='TOKEN',
                         help="A Mapbox token to use when generating a map. If unspecified, the token will be read from "
                              "the MAPBOX_ACCESS_TOKEN or MapboxAccessToken environment variables if set. If no token "
                              "is available, a map will not be displayed.")
+    parser.add_argument('-m', '--measurements', action='store_true',
+                        help="Plot incoming measurement data (slow).")
     parser.add_argument('--no-index', action='store_true',
                         help="Do not automatically open the plots in a web browser.")
     parser.add_argument('-o', '--output', type=str, metavar='DIR',
@@ -1078,8 +1236,9 @@ Load and display information stored in a FusionEngine binary file.
     analyzer.plot_map(mapbox_token=options.mapbox_token)
     analyzer.plot_calibration()
 
-    if options.imu:
+    if options.measurements:
         analyzer.plot_imu()
+        analyzer.plot_wheel_data()
 
     analyzer.generate_event_table()
 
