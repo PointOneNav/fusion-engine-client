a62535c46a3ca780e66c41d665508fa3f0f5e68d
Jonathan Diamond <jonathan@pointonenav.com>
2022-07-22 21:51:00 -0700

    Add definition for output control messages.

--- python/fusion_engine_client/messages/__init__.py
+++ python/fusion_engine_client/messages/__init__.py
@@ -39,6 +39,10 @@ message_type_to_class = {
     SetOutputInterfaceConfigMessage.MESSAGE_TYPE: SetOutputInterfaceConfigMessage,
     GetOutputInterfaceConfigMessage.MESSAGE_TYPE: GetOutputInterfaceConfigMessage,
     OutputInterfaceConfigResponseMessage.MESSAGE_TYPE: OutputInterfaceConfigResponseMessage,
+
+    SetMessageOutputRate.MESSAGE_TYPE: SetMessageOutputRate,
+    GetMessageOutputRate.MESSAGE_TYPE: GetMessageOutputRate,
+    MessageOutputRateResponse.MESSAGE_TYPE: MessageOutputRateResponse,
 }
 
 messages_with_system_time = [t for t, c in message_type_to_class.items() if hasattr(c(), 'system_time_ns')]
