a909bc05db16fe185341b9898ea445fc443d70f8
Adam Shapiro <adam@pointonenav.com>
2022-07-27 19:02:11 -0400

    Allow null/undefined to unregister the callback.

--- wasm/fusion_engine_framer_binding.cc
+++ wasm/fusion_engine_framer_binding.cc
@@ -14,31 +14,35 @@ using namespace point_one::fusion_engine::parsers;
 /******************************************************************************/
 static void RegisterCallback(FusionEngineFramer& framer,
                              const emscripten::val& callback) {
-  // Note: We are intentionally capturing 'callback' by value here so that the
-  // lambda keeps a reference to the caller's function. Otherwise it'll get
-  // destroyed as soon as this function returns.
-  framer.SetMessageCallback(
-      [callback](const MessageHeader& header, const void* payload) {
-        // Create a memory view (i.e., JS Uint8Array) on top of the FusionEngine
-        // message header + payload. In JS, the callback will be called with the
-        // following arguments:
-        // 1. A reference to the MessageHeader struct
-        // 2. A Uint8Array containing the message payload
-        // 3. A Uint8Array containing the complete message (header + payload)
-        //
-        // For example:
-        //   function Callback(header, payload, buffer) {
-        //     // header is a FusionEngine.MessageHeader object
-        //     // ArrayBuffer.isView(payload) returns true
-        //     // ArrayBuffer.isView(buffer) returns true
-        //   }
-        val full_message_view(
-            typed_memory_view(sizeof(MessageHeader) + header.payload_size_bytes,
-                              reinterpret_cast<const uint8_t*>(&header)));
-        val payload_view(typed_memory_view(
-            header.payload_size_bytes, static_cast<const uint8_t*>(payload)));
-        callback(header, payload_view, full_message_view);
-      });
+  if (callback.isUndefined() || callback.isNull()) {
+    framer.SetMessageCallback(nullptr);
+  } else {
+    // Note: We are intentionally capturing 'callback' by value here so that the
+    // lambda keeps a reference to the caller's function. Otherwise it'll get
+    // destroyed as soon as this function returns.
+    framer.SetMessageCallback([callback](const MessageHeader& header,
+                                         const void* payload) {
+      // Create a memory view (i.e., JS Uint8Array) on top of the FusionEngine
+      // message header + payload. In JS, the callback will be called with the
+      // following arguments:
+      // 1. A reference to the MessageHeader struct
+      // 2. A Uint8Array containing the message payload
+      // 3. A Uint8Array containing the complete message (header + payload)
+      //
+      // For example:
+      //   function Callback(header, payload, buffer) {
+      //     // header is a FusionEngine.MessageHeader object
+      //     // ArrayBuffer.isView(payload) returns true
+      //     // ArrayBuffer.isView(buffer) returns true
+      //   }
+      val full_message_view(
+          typed_memory_view(sizeof(MessageHeader) + header.payload_size_bytes,
+                            reinterpret_cast<const uint8_t*>(&header)));
+      val payload_view(typed_memory_view(header.payload_size_bytes,
+                                         static_cast<const uint8_t*>(payload)));
+      callback(header, payload_view, full_message_view);
+    });
+  }
 }
 
 /******************************************************************************/
