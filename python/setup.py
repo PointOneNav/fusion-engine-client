fc5faf43d74a82ef0ac2e057720e49368f11c517
Adam Shapiro <adam@pointonenav.com>
2023-02-06 15:24:43 -0500

    Read the library version from __init__.py in setup.py.

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,23 @@
+import os
+import re
+
 from setuptools import setup, find_packages
+
+
+def find_version(*file_paths):
+    with open(os.path.join(*file_paths), 'rt') as f:
+        version_file = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+        if version_match:
+            return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = find_version('fusion_engine_client', '__init__.py')
 
 setup(
     name='fusion-engine-client',
-    version='v1.15.0',
+    version=version,
     packages=find_packages(where='.'),
     install_requires=[
         'numpy>=1.16.0',
