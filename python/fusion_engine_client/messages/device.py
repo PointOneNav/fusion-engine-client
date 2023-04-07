c79858cb5442a19040edb132ffd49770a18d5236
Adam Shapiro <adam@pointonenav.com>
2023-04-07 12:57:36 -0400

    Use FixedPointAdapter for SystemStatusMessage.

--- python/fusion_engine_client/messages/device.py
+++ python/fusion_engine_client/messages/device.py
@@ -2,7 +2,7 @@ import math
 
 from construct import (Struct, Int16sl, Padding)
 
-from ..utils.construct_utils import construct_message_to_string
+from ..utils.construct_utils import FixedPointAdapter, construct_message_to_string
 from .defs import *
 
 
@@ -13,11 +13,9 @@ class SystemStatusMessage(MessagePayload):
     MESSAGE_TYPE = MessageType.SYSTEM_STATUS
     MESSAGE_VERSION = 0
 
-    INVALID_TEMPERATURE = -32768
-
     SystemStatusMessageConstruct = Struct(
         "p1_time" / TimestampConstruct,
-        "gnss_temperature" / Int16sl,
+        "gnss_temperature_degc" / FixedPointAdapter(2 ** -3, Int16sl, invalid=0x8000),
         Padding(118),
     )
 
@@ -27,32 +25,18 @@ class SystemStatusMessage(MessagePayload):
 
     def pack(self, buffer: bytes = None, offset: int = 0, return_buffer: bool = True) -> (bytes, int):
         values = vars(self)
-
-        if math.isnan(values['gnss_temperature_degc']):
-            values['gnss_temperature'] = SystemStatusMessage.INVALID_TEMPERATURE
-        else:
-            values['gnss_temperature'] = int(round(values['gnss_temperature_degc'] * 8))
-        del values['gnss_temperature_degc']
-
         packed_data = self.SystemStatusMessageConstruct.build(values)
         return PackedDataToBuffer(packed_data, buffer, offset, return_buffer)
 
     def unpack(self, buffer: bytes, offset: int = 0, message_version: int = MessagePayload._UNSPECIFIED_VERSION) -> int:
         parsed = self.SystemStatusMessageConstruct.parse(buffer[offset:])
-
-        if parsed['gnss_temperature'] == SystemStatusMessage.INVALID_TEMPERATURE:
-            parsed['gnss_temperature_degc'] = math.nan
-        else:
-            parsed['gnss_temperature_degc'] = parsed['gnss_temperature'] / 8
-        del parsed['gnss_temperature']
-
         self.__dict__.update(parsed)
         del self.__dict__['_io']
         return parsed._io.tell()
 
     def __repr__(self):
         result = super().__repr__()[:-1]
-        result += f', gnss_temperature={self.gnss_temperature}]'
+        result += f', gnss_temperature={self.gnss_temperature_degc:.1f} deg C]'
         return result
 
     def __str__(self):
