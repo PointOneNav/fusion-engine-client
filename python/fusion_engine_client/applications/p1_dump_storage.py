7c8faa36f5ebf6318aa78c4c609b49ca9e832dff
Adam Shapiro <adam@pointonenav.com>
2023-10-24 10:11:14 -0400

    Allow internal applications to be run as scripts.

--- python/fusion_engine_client/applications/p1_dump_storage.py
+++ python/fusion_engine_client/applications/p1_dump_storage.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
+if __package__ is None or __package__ == "":
+    from import_utils import enable_relative_imports
+    __package__ = enable_relative_imports(__name__, __file__)
+
 from ..messages import *
 from ..parsers import MixedLogReader
 from ..utils import trace as logging
