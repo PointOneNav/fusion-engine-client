33ac81ecbce67751e64af874c280fda6240483d4
Adam Shapiro <adam@pointonenav.com>
2023-11-01 09:22:24 -0400

    Added enable_relative_imports() helper function for application scripts.

--- python/fusion_engine_client/applications/p1_lband_extract.py
+++ python/fusion_engine_client/applications/p1_lband_extract.py
@@ -3,13 +3,9 @@
 import os
 import sys
 
-# If this application is being run directly as a script (e.g., python p1_*.py), rather than as a script installed by pip
-# (e.g., p1_*), manually set the package name and add the Python root directory (fusion-engine-client/python/) to the
-# import search path to enable relative imports.
-if __name__ == "__main__":
-    root_dir = os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../..'))
-    sys.path.insert(0, root_dir)
-    __package__ = os.path.dirname(__file__).replace('/', '.')
+if __package__ is None or __package__ == "":
+    from import_utils import enable_relative_imports
+    __package__ = enable_relative_imports(__name__, __file__)
 
 from ..utils.trace import HighlightFormatter, BrokenPipeStreamHandler
 from ..utils.time_range import TimeRange
