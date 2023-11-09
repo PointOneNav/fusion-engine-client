9f52679006375f4facd2a20155ac62c249ff8f3c
Adam Shapiro <adam@pointonenav.com>
2023-11-09 11:08:38 -0500

    Added p1_print --format={binary,pretty-binary,oneline-binary} options.

--- python/fusion_engine_client/utils/bin_utils.py
+++ python/fusion_engine_client/utils/bin_utils.py
@@ -3,12 +3,15 @@ import textwrap
 
 def bytes_to_hex(buffer, bytes_per_row=32, bytes_per_col=1):
     if bytes_per_col == 1:
-        return '\n'.join(textwrap.wrap(' '.join(['%02X' % b for b in buffer]), (3 * bytes_per_row - 1)))
+        if bytes_per_row > 0:
+            return '\n'.join(textwrap.wrap(' '.join(['%02X' % b for b in buffer]), (3 * bytes_per_row - 1)))
+        else:
+            return ' '.join(['%02X' % b for b in buffer])
     else:
         byte_string = ''
         for i, b in enumerate(buffer):
             if i > 0:
-                if (i % bytes_per_row) == 0:
+                if bytes_per_row > 0 and (i % bytes_per_row) == 0:
                     byte_string += '\n'
                 elif (i % bytes_per_col) == 0:
                     byte_string += ' '
