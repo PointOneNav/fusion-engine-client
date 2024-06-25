b1181867feb10524caa2ce1d12489ea4ef4adf50
Adam Shapiro <adam@pointonenav.com>
2024-06-25 10:42:23 -0400

    Added enabled bool to all transport config structs.

--- src/point_one/fusion_engine/messages/configuration.h
+++ src/point_one/fusion_engine/messages/configuration.h
@@ -2242,8 +2242,8 @@ inline p1_ostream& operator<<(p1_ostream& stream, MessageRate val) {
  * @ingroup io_interfaces
  */
 struct P1_ALIGNAS(4) TCPConfig {
+  uint8_t enabled = 1;
   TransportDirection direction = TransportDirection::SERVER;
-  uint8_t reserved[1] = {0};
   uint16_t port = 0;
   char remote_address[64] = {0};
 };
@@ -2253,7 +2253,8 @@ struct P1_ALIGNAS(4) TCPConfig {
  * @ingroup io_interfaces
  */
 struct P1_ALIGNAS(4) UDPConfig {
-  uint8_t reserved[2] = {0};
+  uint8_t enabled = 1;
+  uint8_t reserved[1] = {0};
   uint16_t port = 0;
   char remote_address[64] = {0};
 };
@@ -2263,8 +2264,8 @@ struct P1_ALIGNAS(4) UDPConfig {
  * @ingroup io_interfaces
  */
 struct P1_ALIGNAS(4) WebsocketConfig {
+  uint8_t enabled = 1;
   TransportDirection direction = TransportDirection::SERVER;
-  uint8_t reserved[1] = {0};
   uint16_t port = 0;
   char remote_address[64] = {0};
 };
@@ -2274,9 +2275,10 @@ struct P1_ALIGNAS(4) WebsocketConfig {
  * @ingroup io_interfaces
  */
 struct P1_ALIGNAS(4) UNIXSocketConfig {
+  uint8_t enabled = 1;
   TransportDirection direction = TransportDirection::SERVER;
   UNIXSocketType socket_type = UNIXSocketType::STREAM;
-  uint8_t reserved[2] = {0};
+  uint8_t reserved[1] = {0};
   char path[64] = {0};
 };
 
@@ -2285,6 +2287,8 @@ struct P1_ALIGNAS(4) UNIXSocketConfig {
  * @ingroup io_interfaces
  */
 struct P1_ALIGNAS(4) SerialConfig {
+  uint8_t enabled = 1;
+  uint8_t reserved[3] = {0};
   uint32_t baud_rate = 0;
   char device_path[64] = {0};
 };
