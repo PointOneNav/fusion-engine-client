77f526b09abfc50797e876c3ede0aea53c190a8d
ericschorling <eschorling@pointonenav.com>
2024-12-16 12:40:55 -0500

    Fixed styling.

--- wasm/build.sh
+++ wasm/build.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-    ################################################################################
-#Set Directory Locations
-    ################################################################################
+################################################################################
+# Set Directory Locations
+################################################################################
 
-#Find the directory of this file, following symlinks.
+# Find the directory of this file, following symlinks.
 #
-#Reference:
-#- https: //stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-    get_parent_dir() {
+# Reference:
+# - https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+get_parent_dir() {
     local SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SOURCE" ]; do
-      local DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-      SOURCE="$(readlink "$SOURCE")"
-      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+        local DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
     done
 
     local PARENT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
