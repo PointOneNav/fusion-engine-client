fe4a5aafad821b6091b7c8e36dd4f1141bc20403
Adam Shapiro <adam@pointonenav.com>
2026-02-16 11:17:01 -0500

    Added file I/O support to configure_transport().

--- python/fusion_engine_client/utils/transport_utils.py
+++ python/fusion_engine_client/utils/transport_utils.py
@@ -1,6 +1,7 @@
 import re
 import socket
-from typing import Callable, Union
+import sys
+from typing import Callable, TextIO, Union
 
 # WebSocket support is optional. To use, install with:
 #   pip install websockets
@@ -38,7 +39,53 @@ except ImportError:
         class Serial: pass
         class SerialException(Exception): pass
 
+class FileTransport:
+    def __init__(self, input: Union[str, TextIO] = None, output: Union[str, TextIO] = None):
+        if isinstance(input, str):
+            if input in ('', '-'):
+                self.input = sys.stdin.buffer
+                self.input_path = 'stdin'
+            else:
+                self.input = open(input, 'rb')
+                self.input_path = input
+        else:
+            self.input = input
+            self.input_path = input.name if input else None
+
+        if isinstance(output, str):
+            if output in ('', '-'):
+                self.output = sys.stdout.buffer
+                self.output_path = 'stdout'
+            else:
+                self.output = open(output, 'wb')
+                self.output_path = output
+        else:
+            self.output = output
+            self.output_path = output.name if output else None
+
+    def close(self):
+        if self.input is not None and self.input is not sys.stdin.buffer:
+            self.input.close()
+        if self.output is not None and self.output is not sys.stdout.buffer:
+            self.output.close()
+
+    def read(self, size: int = -1) -> bytes:
+        if self.input:
+            return self.input.read(size)
+        else:
+            raise RuntimeError('Input file not opened.')
+
+    def write(self, data: Union[bytes, bytearray]) -> int:
+        if self.output:
+            return self.output.write(data)
+        else:
+            raise RuntimeError('Output file not opened.')
+
+
 TRANSPORT_HELP_OPTIONS = """\
+- <empty string> - Read from stdin and/or write to stdout
+- [file://](PATH|-) - Read from/write to the specified file, or to stdin/stdout
+  if PATH is '-'.
 - tcp://HOSTNAME[:PORT] - Connect to the specified hostname (or IP address) and
   port over TCP (e.g., tty://192.168.0.3:30202); defaults to port 30200.
 - udp://:PORT - Listen for incoming data on the specified UDP port (e.g.,
@@ -57,8 +104,43 @@ The method used to communicate with the target device:
 {TRANSPORT_HELP_OPTIONS}
 """
 
-def create_transport(descriptor: str, timeout_sec: float = None, print_func: Callable = None) -> \
-        Union[socket.socket, serial.Serial, ws.ClientConnection]:
+
+def create_transport(descriptor: str, timeout_sec: float = None, print_func: Callable = None, mode: str = 'both',
+                     stdout=sys.stdout) -> \
+        Union[socket.socket, serial.Serial, ws.ClientConnection, FileTransport]:
+    # File: path, '-' (stdin/stdout), empty string (stdin/stdout)
+    if descriptor in ('', '-'):
+        descriptor = 'file://-'
+
+    m = re.match(r'^(?:file://)?([a-zA-Z0-9-_./]+)$', descriptor)
+    if m:
+        path = m.group(1)
+        if mode == 'both':
+            if path != '-':
+                raise ValueError("Cannot open a file for both read and write access.")
+
+            if print_func is not None:
+                print_func(f'Connecting to stdin/stdout.')
+            transport = FileTransport(input='-', output='-')
+        elif mode == 'input':
+            if print_func is not None:
+                if path == '-':
+                    print_func(f'Reading from stdin.')
+                else:
+                    print_func(f'Reading from {path}.')
+            transport = FileTransport(input=path, output=None)
+        elif mode == 'output':
+            if print_func is not None:
+                if path == '-':
+                    print_func(f'Writing to stdout.')
+                else:
+                    print_func(f'Writing to {path}.')
+            transport = FileTransport(input=None, output=stdout.buffer)
+        else:
+            raise ValueError(f"Unsupported file mode '{mode}'.")
+        return transport
+
+    # TCP client
     m = re.match(r'^tcp://([a-zA-Z0-9-_.]+)?(?::([0-9]+))?$', descriptor)
     if m:
         hostname = m.group(1)
@@ -76,6 +158,7 @@ def create_transport(descriptor: str, timeout_sec: float = None, print_func: Cal
             raise socket.timeout(f'Timed out connecting to tcp://{ip_address}:{port}.')
         return transport
 
+    # UDP client
     m = re.match(r'^udp://:([0-9]+)$', descriptor)
     if m:
         port = int(m.group(1))
@@ -89,6 +172,7 @@ def create_transport(descriptor: str, timeout_sec: float = None, print_func: Cal
         transport.bind(('', port))
         return transport
 
+    # Websocket client
     m = re.match(r'^ws://([a-zA-Z0-9-_.]+):([0-9]+)$', descriptor)
     if m:
         hostname = m.group(1)
@@ -110,6 +194,7 @@ def create_transport(descriptor: str, timeout_sec: float = None, print_func: Cal
             raise TimeoutError(f'Timed out connecting to {url}.')
         return transport
 
+    # UNIX domain socket
     m = re.match(r'^unix://([a-zA-Z0-9-_./]+)$', descriptor)
     if m:
         path = m.group(1)
@@ -122,7 +207,8 @@ def create_transport(descriptor: str, timeout_sec: float = None, print_func: Cal
         transport.connect(path)
         return transport
 
-    m = re.match(r'^(?:(?:serial|tty)://)?([^:]+)(?::([0-9]+))?$', descriptor)
+    # Serial port
+    m = re.match(r'^(?:(?:serial|tty)://)?([^:]+)(?::([0-9]+))$', descriptor)
     if m:
         path = m.group(1)
         if m.group(2) is None:
