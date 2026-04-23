34b825338a21f7b771d78797683ae56984f261e7
Adam Shapiro <adam@pointonenav.com>
2026-04-23 18:53:10 -0400

    Added allow_multiple option to find_matching_values().

--- python/fusion_engine_client/utils/enum_utils.py
+++ python/fusion_engine_client/utils/enum_utils.py
@@ -115,7 +115,7 @@ class IntEnum(IntEnumBase, metaclass=DynamicEnumMeta):
 
     @classmethod
     def find_matching_values(cls, pattern: Union[int, str, List[int], List[str]], raise_on_unrecognized: bool = False,
-                             prefix: str = None, print_func=None) -> Set['IntEnum']:
+                             prefix: str = None, allow_multiple: bool = True, print_func=None) -> Set['IntEnum']:
         """!
         @brief Find one or more enum values that match the specified pattern(s).
 
@@ -145,6 +145,7 @@ class IntEnum(IntEnumBase, metaclass=DynamicEnumMeta):
         @param raise_on_unrecognized If `True`, raise an exception for any unrecognized integer values. Unrecognized
                values will automatically create new enum entries by default.
         @param prefix If specified, prepend the prefix to all string values if they do not already start with it.
+        @param allow_multiple If `True`, allow multiple matches when the pattern contains a wildcard.
 
         @return A set containing the matching enum values.
         """
@@ -163,6 +164,8 @@ class IntEnum(IntEnumBase, metaclass=DynamicEnumMeta):
         #   ['VersionInfoMessage', 'PoseMessage,GNSS*'] ->
         #   ['VersionInfoMessage', 'PoseMessage', 'GNSS*']
         requested_types = [p.strip() for entry in patterns for p in entry.split(',')]
+        if len(requested_types) > 1 and not allow_multiple:
+            raise ValueError("Multiple type specifiers not permitted.")
 
         # Now find matches to each pattern.
         result = set()
@@ -178,10 +181,14 @@ class IntEnum(IntEnumBase, metaclass=DynamicEnumMeta):
                 if str(enum_val) == '(Unrecognized)' and print_func:
                     print_func(f"{pattern} is an unknown {cls.__name__} value.")
             except:
+                original_pattern = pattern
                 if prefix is not None and not pattern.startswith(prefix):
                     pattern = f'{prefix}*{pattern}'
 
-                allow_multiple = '*' in pattern
+                if allow_multiple:
+                    has_wildcard = '*' in pattern
+                else:
+                    has_wildcard = False
                 re_pattern = pattern.replace('*', '.*')
                 # if pattern[0] != '^':
                 #     re_pattern = r'.*' + re_pattern
@@ -192,14 +199,18 @@ class IntEnum(IntEnumBase, metaclass=DynamicEnumMeta):
                 matched_types = [v for k, v in cls._member_map_.items()
                                 if re.match(re_pattern, k, flags=re.IGNORECASE)]
                 if len(matched_types) == 0 and print_func:
-                    print_func("No message types matching pattern '%s'." % pattern)
+                    print_func("No message types matching pattern '%s'." % original_pattern)
                     continue
 
                 # If there are too many matches, fail.
-                if len(matched_types) > 1 and not allow_multiple:
-                    raise ValueError("Pattern '%s' matches multiple message types:%s\n\nAdd a wildcard (%s*) to display "
-                                     "all matching types." %
-                                     (pattern, ''.join(['\n  %s' % c for c in matched_types]), pattern))
+                if len(matched_types) > 1 and not has_wildcard:
+                    if allow_multiple:
+                        wildcard_str = f"\n\nAdd a wildcard ({original_pattern}*) to display all matching types."
+                    else:
+                        wildcard_str = ''
+                    raise ValueError("Pattern '%s' matches multiple message types:%s%s" %
+                                     (original_pattern, ''.join(['\n  %s' % c for c in matched_types]),
+                                      wildcard_str))
                 # Otherwise, update the set of message types.
                 else:
                     result.update(matched_types)
