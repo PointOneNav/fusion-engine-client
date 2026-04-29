cd80581c38896ec8de26dc51a9ae87b96a7b3e57
Adam Shapiro <adam@pointonenav.com>
2026-04-29 09:04:26 -0400

    Added helper functions to Timestamp class.

--- src/point_one/fusion_engine/messages/timestamp.h
+++ src/point_one/fusion_engine/messages/timestamp.h
@@ -33,6 +33,62 @@ struct P1_ALIGNAS(4) Timestamp {
 
   /** The fractional part of the second, expressed in nanoseconds. */
   uint32_t fraction_ns = INVALID;
+
+  /**
+   * @brief Check if this is a valid timestamp.
+   *
+   * @return `true` if the timestamp is valid.
+   */
+  bool IsValid() const { return seconds != INVALID && fraction_ns != INVALID; }
+
+  /**
+   * @brief Get the timestamp value in seconds.
+   *
+   * @return The timestamp value (in seconds), or `NAN` if invalid.
+   */
+  double ToSeconds() const {
+    if (IsValid()) {
+      return seconds + (fraction_ns * 1e-9);
+    } else {
+      return NAN;
+    }
+  }
+
+  /**
+   * @brief Convert a timestamp to GPS week number and time of week.
+   *
+   * @param week_number Set to the GPS week number.
+   * @param tow_sec Set to the GPS time of week (in seconds).
+   *
+   * @return `true` on success, `false` if the timestamp is invalid.
+   */
+  bool ToGPSWeekTOW(uint16_t* week_number, double* tow_sec) const {
+    if (IsValid()) {
+      if (week_number) {
+        *week_number = static_cast<uint16_t>(seconds / 604800);
+      }
+      if (tow_sec) {
+        *tow_sec = (seconds % 604800) + (fraction_ns * 1e-9);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * @brief Check if this is valid timestamp.
+   *
+   * @return `true` if the timestamp is valid.
+   */
+  operator bool() const { return IsValid(); }
+
+  /**
+   * @brief Get the timestamp value in seconds.
+   *
+   * @return The timestamp value (in seconds), or `NAN` if invalid.
+   */
+  operator double() const { return ToSeconds(); }
 };
 
 } // namespace messages
